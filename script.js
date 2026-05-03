76368db675e5e6161a14c6165695451294860104
Skinnyenxis <155930434+Skinnyenxis@users.noreply.github.com>
2026-05-04 00:12:15 +0500

    Update greeting text to Russian

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "знаешь что! Ты самая прекрасная девушка которую я когда либо встречал 💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
